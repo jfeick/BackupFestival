633ddf749b184f603867ad377f8e4f53a8a4b3b5
Jan Frederick Eick <j.f.eick@gmx.de>
2015-04-29 10:53:33 +0200

    Initial commit

---
+++ app/src/main/java/de/uni_weimar/m18/backupfestival/views/adapters/ImageAdapter.java
@@ -0,0 +1,19 @@
+package de.uni_weimar.m18.backupfestival.views.adapters;
+
+/**
+ * Created by Jan Frederick Eick on 29.04.2015.
+ * Copyright 2015 Jan Frederick Eick
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class ImageAdapter {
+}
